5be4062d75442f5be55f1bc5a5fe0e15e263a37b
d0777 <105343207+d0777@users.noreply.github.com>
2022-06-15 14:50:12 +0530

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,18 +1,38 @@
-def call(String mvnaction) {
-    
-       if ("${mvnaction}" == "Clean")
-                    {
-                    sh "mvn clean"
-                    }
-              else if ("${mvnaction}" == "Compile")
-                    {
-                    sh "mvn clean compile"
-                    }
-               else if ("${mvnaction}" == "Test")
-                    {
-                    sh "mvn clean test"
-                    }
-                 else if ("${mvnaction}" == "Install")
-                    {
-                    sh "mvn clean install"
-                    }
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       tools {
+           maven 'Maven 3.5.0'
+           jdk 'jdk8'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
+}
